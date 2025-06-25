3af0cff8d3a7a41f2af7209ae6de4a03fd98b9dc
ashhanai <ashhab.naim@gmail.com>
2025-06-25 12:12:29 +0200

    test lender direct repayment hook

--- src/periphery/loan/hook/lender/PWNDirectLenderRepaymentHook.sol
+++ src/periphery/loan/hook/lender/PWNDirectLenderRepaymentHook.sol
@@ -6,15 +6,28 @@ import { MultiToken } from "MultiToken/MultiToken.sol";
 import { IPWNLenderRepaymentHook, LENDER_REPAYMENT_HOOK_RETURN_VALUE } from "pwn/core/loan/hook/IPWNLenderRepaymentHook.sol";
 
 
+/**
+ * @title PWNDirectLenderRepaymentHook
+ * @notice Lender repayment hook that directly transfers the repayment to the lender.
+ * @dev This hook is used to forward the repayment amount to the lender's address without any additional logic.
+ */
 contract PWNDirectLenderRepaymentHook is IPWNLenderRepaymentHook {
     using MultiToken for address;
     using MultiToken for MultiToken.Asset;
 
+    /** @notice Thrown when the lender address is zero.*/
     error LenderZeroAddress();
+    /** @notice Thrown when the credit asset address is zero.*/
     error CreditZeroAddress();
+    /** @notice Thrown when the repayment amount is zero.*/
     error RepaymentZero();
+    /** @notice Thrown when the lender data is not empty.*/
     error DataNotEmpty();
 
+    /**
+     * @notice Called on loan repayment to transfer repayment directly to the lender.
+     * @inheritdoc IPWNLenderRepaymentHook
+     */
     function onLoanRepaid(
         address lender,
         address creditAddress,
@@ -27,6 +40,7 @@ contract PWNDirectLenderRepaymentHook is IPWNLenderRepaymentHook {
         if (lenderData.length != 0) revert DataNotEmpty();
 
         creditAddress.ERC20(repayment).transferAssetFrom(address(this), lender);
+
         return LENDER_REPAYMENT_HOOK_RETURN_VALUE;
     }
 
---
+++ test/unit/PWNDirectLenderRepaymentHook.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.16;
+
+import { Test } from "forge-std/Test.sol";
+
+import {
+    PWNDirectLenderRepaymentHook,
+    IPWNLenderRepaymentHook, LENDER_REPAYMENT_HOOK_RETURN_VALUE
+} from "pwn/periphery/loan/hook/lender/PWNDirectLenderRepaymentHook.sol";
+
+
+abstract contract PWNDirectLenderRepaymentHookTest is Test {
+
+    PWNDirectLenderRepaymentHook hook;
+    address lender = makeAddr("lender");
+    address creditAddress = makeAddr("creditAddress");
+
+
+    function setUp() public virtual {
+        hook = new PWNDirectLenderRepaymentHook();
+
+        vm.mockCall(creditAddress, abi.encodeWithSignature("transfer(address,uint256)"), abi.encode(true));
+    }
+
+}
+
+
+/*----------------------------------------------------------*|
+|*  # ON LOAN REPAID                                        *|
+|*----------------------------------------------------------*/
+
+contract PWNDirectLenderRepaymentHook_OnLoanRepaid_Test is PWNDirectLenderRepaymentHookTest {
+
+    function test_shouldFail_whenMissingInputs() external {
+        vm.expectRevert(PWNDirectLenderRepaymentHook.LenderZeroAddress.selector);
+        hook.onLoanRepaid(address(0), creditAddress, 1, "");
+
+        vm.expectRevert(PWNDirectLenderRepaymentHook.CreditZeroAddress.selector);
+        hook.onLoanRepaid(lender, address(0), 1, "");
+
+        vm.expectRevert(PWNDirectLenderRepaymentHook.RepaymentZero.selector);
+        hook.onLoanRepaid(lender, creditAddress, 0, "");
+
+        vm.expectRevert(PWNDirectLenderRepaymentHook.DataNotEmpty.selector);
+        hook.onLoanRepaid(lender, creditAddress, 1, abi.encode(1));
+    }
+
+    function testFuzz_shouldTransferToLender(uint256 repayment) external {
+        repayment = bound(repayment, 1, type(uint256).max);
+
+        vm.expectCall(creditAddress, abi.encodeWithSignature("transfer(address,uint256)", lender, repayment));
+
+        hook.onLoanRepaid(lender, creditAddress, repayment, "");
+    }
+
+    function test_shouldReturnHookValue() external {
+        assertEq(
+            hook.onLoanRepaid(lender, creditAddress, 1, ""),
+            LENDER_REPAYMENT_HOOK_RETURN_VALUE
+        );
+    }
+
+}
